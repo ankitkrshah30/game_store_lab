4fe04edf3fbe2df542efa38b08e78d49d55089d8
Ankit Kumar Shah <ankitkrshah30@gmail.com>
2025-09-19 15:00:22 +0530

    added WebConfig.java

---
+++ src/main/java/com/supercell/gaming_store/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.supercell.gaming_store.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**") // Apply this rule to all API endpoints
+                .allowedOrigins("*") // Allow requests from ANY origin (frontend)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
+                .allowedHeaders("*") // Allowed request headers
+                .allowCredentials(true);
+    }
+}
--- src/main/java/com/supercell/gaming_store/service/impl/GamesServiceImpl.java
+++ src/main/java/com/supercell/gaming_store/service/impl/GamesServiceImpl.java
@@ -3,11 +3,13 @@ package com.supercell.gaming_store.service.impl;
 import com.supercell.gaming_store.entity.Games;
 import com.supercell.gaming_store.repository.GamesRepository;
 import com.supercell.gaming_store.service.interfaces.GamesService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class GamesServiceImpl implements GamesService {
 
 
@@ -20,6 +22,7 @@ public class GamesServiceImpl implements GamesService {
     @Override
     public Games addGame(Games game) {
         // Here you could add logic to check if a game with the same name already exists
+        log.info("Adding game with name: {}", game.getName());
         return gamesRepository.save(game);
     }
 
