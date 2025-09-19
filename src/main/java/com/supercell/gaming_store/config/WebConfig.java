48a026ae960461eab57c6dd6d8a32eadaf53c812
Ankit Kumar Shah <ankitkrshah30@gmail.com>
2025-09-19 16:26:43 +0530

    added resourceHandler

--- src/main/java/com/supercell/gaming_store/config/WebConfig.java
+++ src/main/java/com/supercell/gaming_store/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.supercell.gaming_store.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -16,5 +17,11 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
                 .allowedHeaders("*") // Allowed request headers
                 .allowCredentials(false);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("classpath:/static/GIF_Assets/");
     }
 }
